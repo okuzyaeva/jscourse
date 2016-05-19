107790e1ea92f479b77a4cf03353c894c6e82185
lightest <lightest13@gmail.com>
2016-05-19 14:36:36 +0300

    reworking tree

--- lesson7/system.js
+++
@@ -1,9 +0,0 @@
-var system = {
-    touchEvent: 'click',
-
-    init: function () {
-        // if(navigator.userAgent.indexOf('AppleWebKit') !== -1 && navigator.userAgent.indexOf('Mobile') !== -1) {
-        //     this.touchEvent = 'touchend';
-        // }
-    }
-};
