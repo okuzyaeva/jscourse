f281cc02b5d60858ec22d2a83798b0976fe56c17
lightest <lightest13@gmail.com>
2016-05-29 17:45:36 +0300

    l10 commentaries

---
+++ lesson10/README.md
@@ -0,0 +1,20 @@
+Lesson10:
+---------
+As requested by attendees we've covered all the basic aspects of JS that were used during the course plus some other basic info.
+
+Topics covered:
+
+- Primitive data types
+- null vs undefined
+- typeof, instanceof
+- typeof(NaN)
+- Division by 0
+- Arithmetics of strings
+- Arrays
+- Object literals
+- Prototypal inheritance
+- Thoughts on future of the web
+
+Homework:
+---------
+Prepare yourself for a test.
--- lesson10/logic.js
+++ lesson10/logic.js
@@ -1,27 +1,107 @@
-var obj;
+// five primitives
+var n = 5;
+var str = 'str';
+var b = true;
+var obj = null;
+var idontknow = undefined;
+// variables above passed to functions by value (copied) not by reference
+// they also immutable which means when you type n = 10 a new memory is allocated
+// and old value (5) is thrown away.
 
-// function User () {
-//     this.name = 'u1';
-// }
+// number string and boolean have so called "object wrappers" which means
+// they wrap around a primitive and provide various methods.
+n = new Number(5); // contains primitive 5 and functions like .toFixed()
+// without new keyword they can be used as type converters e.g:
+n = Number(''); // n = 0;
+// you can read in depth about Primitives here https://developer.mozilla.org/ru/docs/Glossary/Primitive
 
-// User.prototype.getName = function () {
-//     return this.name;
-// };
+// arrays
+var arr = [];        // array is defined like this
+arr = new Array();   // or like this
+arr = new Array(10); // creates array with 10 undefined cells;
 
-// function Administrator () {
-//     'use strict';
+arr.push(1);  // adds 1 to tail
+arr.pop();    // extracts tail value
+arr[10] = 11; // sets 10th element to 11
 
-//     User.call(this);
-//     this.admin = true;
-// }
+// iterate arrays like this:
+arr.forEach(function (item, index) { /* do something */ });
+for(var i in arr) { /* do something */ };
+for(var i = arr.length; --i >= 0;) { /* do something */ }; // handy if you want remove i element
 
-// Administrator.prototype = Object.create(User.prototype);
+arr.splice(3, 1); // removes 1 element at index 3;
+arr.shift();      // extracts element from beginning;
+arr.unshift(3);   // inserts 3 in the beginning of array;
+// in depth: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/array
 
-class User {
+// object literal
+obj = {};
+obj.a = 10; // it's possible to add fields on the fly
+// for tricky stuff:
+Object.defineProperty(obj, 'propertyname', {
+    writable: false,
+    value: 'hey this is not writable!'
+});
+// by accessing obj.propertyNameGetter we'll receive value above
+Object.defineProperty(obj, 'propertyNameGetter', {
+    get: function () {
+        return this.propertyname;
+    }
+});
+// more about defineProperty and defineProperties syntax here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
+
+// iterate objects like this:
+for(var i in obj) {
+    /* where i is the property name, access like array obj[i] */
+}
+// note that iterator above also will access the whole prototype chain
+// prototype of the object can be set like this:
+obj.__proto__ = {b: 7};
+
+// to check if object itself has a property or it's actually on the prototype use:
+obj.hasOwnProperty('b');
+
+// inheritance ES5 way -----------------------------------
+// step 1: define User (parent) constructor function
+
+function User () {
+    this.name = 'u1';
+}
+
+// step 2: extend it's prototype with required functions
+
+User.prototype.getName = function () {
+    return this.name;
+};
+
+// step 3: define descendant constructor function
+
+function Administrator () {
+    'use strict';
+
+    User.call(this); // call parent function to create parent's properties
+    this.admin = true;
+}
+
+// step 4: set descendant's prototype to be object whos prototype is the prototype
+//         of User. This will allow for prototype chain to appear.
+Administrator.prototype = Object.create(User.prototype);
+
+// step 5: set prototype's constructor field to appropriate function constructor.
+//         have no idea what it affects, but it's important to preserver consistensy.
+//         In other words by referencing .constructor field of the instance I want
+//         to get its constructor function, not its parent's.
+Administrator.prototype.constructor = Administrator;
+
+// inheritance ES6 way -----------------------------------
+
+class User_1 {
+    // constructor acts just like ES5 constructor function
     constructor () {
         this._name = 'u1';
     }
 
+    // all functions below defined on a prototype of User_1
     getName () {
         return this._name;
     }
@@ -35,9 +115,10 @@ class User {
     }
 }
 
-class Administrator extends User {
+// extends allows to create prototypal inheritance
+class Administrator_1 extends User_1 {
     constructor () {
-        super();
+        super(); // invokes User_1 constructor
         this.admin = true;
         this.arr = [1,2,3,4];
     }
@@ -49,8 +130,12 @@ class Administrator extends User {
         
         console.log('index', i);
     }
+
+    set name (n) {
+        // it's also possible to overload parent's functions
+        super.name = name; // use super to reference original functions
+        console.log('new administrator name is', this._name);
+    }
 }
 
-window.onload = function () {
-
-}
+// more ES6 features are here: https://github.com/lukehoban/es6features
